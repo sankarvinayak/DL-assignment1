fb8740046cc03c26baf62d86bfbf06940e37fdf1
Sankar Vinayak E P <64238983+sankarvinayak@users.noreply.github.com>
2025-02-25 17:23:35 +0530

    cli arguments handling

--- train.py
+++ train.py
@@ -1,5 +1,24 @@
 from src.cli.options import get_args()
 def main():
-    args = get_args()
+  args = get_args()
+  wandb_entity=args.wandb_entity
+  wandb_project=args.wandb_project
+  dataset=arg.dataset
+  epoch=args.epoch
+  batch_size=args.batch_size
+  loss_fn=args.loss_fn
+  lr=args.lr
+  momentum=args.momentum
+  beta=args.beta
+  beta1=args.beta1
+  beta2=args.beta2
+  epsilon=args.epsilon
+  lmda=args.decay
+  weight_init=args.weight_init
+  optimizer=args.optimizer
+  activation_fn=args.activation_fn
+  num_hidden=args.num_hidden
+  hidden_size=args.hidden_size
+
 if __name__ == '__main__':
     main()
