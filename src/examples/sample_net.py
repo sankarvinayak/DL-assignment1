1c5f849cbd1b5b146e36d0454cf29faf628be630
Sankar Vinayak E P <64238983+sankarvinayak@users.noreply.github.com>
2025-03-09 09:35:05 +0530

    Update sample_net.py

--- src/examples/sample_net.py
+++ src/examples/sample_net.py
@@ -12,6 +12,8 @@ def main():
     out_shp=np.unique(y_train).shape[0] 
     
     model=construct_network(inp_size=inp_shp,num_layers=3,layer_size=32,out_size=out_shp) # Create a 1 input layer 2 hidden layer 1 output layer network due to the softmax layer will give output probabilities
+    print(model.forward_pass_network(x_train_flat)) #return probability over 10 classes
+    
     train_loop(x_train_flat,y_train,max_iter=5,model=model,print_iter=1)
 
 
